e5fa8957b89f9bb6e16440f442904af14b519fee
Joel Ringh <joel.ringh@gmail.com>
2025-09-15 11:44:57 +0200

    Miniräknare Skapad

--- Ny Miniräknare/Program.cs
+++ Ny Miniräknare/Program.cs
@@ -2,9 +2,67 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string räknesätt;
+            int a = 0, b = 0;
+
+            Console.WriteLine("Vilket räknesätt vill du använa? \"+-*/\"?");
+            räknesätt = Console.ReadLine();
+
+            Console.WriteLine("Vilka nummer vill du räkna med?");
+
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("och?");
+
+                b = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ogiltigt");
+                Main();
+            }
+
+            switch (räknesätt)
+            {
+                case "+":
+                    Addition(a, b);
+                    break;
+                case "-":
+                    Subtraktion(a, b);
+                    break;
+                case "*":
+                    Multiplikation(a, b);
+                    break;
+                case "/":
+                    Division(a, b);
+                    break;
+                default:
+                    Console.WriteLine("Ogiltigt räknesätt");
+                    break;
+            }
+
+            void Addition(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+
+            void Subtraktion(int a, int b)
+            {
+                Console.WriteLine(a - b);
+            }
+
+            void Multiplikation(int a, int b)
+            {
+                Console.WriteLine(a * b);
+            }
+
+            void Division(int a, int b)
+            {
+                Console.WriteLine(a / b);
+            }
         }
     }
 }
