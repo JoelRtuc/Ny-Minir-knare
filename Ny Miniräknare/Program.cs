b37276c115641888e9276c1511541beacddd2707
Joel Ringh <joel.ringh@gmail.com>
2025-09-15 11:31:03 +0200

    Add project files.

---
+++ Ny Miniräknare/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ny_Miniräknare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
